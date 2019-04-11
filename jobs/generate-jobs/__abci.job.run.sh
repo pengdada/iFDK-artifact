4be9b840fdc4f3385f5048ee4ae96fd452a5e3af
chen peng <chinhou0718@gmail.com>
2019-04-11 11:20:56 +0900

    Update and rename __abci.job.run.strong.sh to __abci.job.run.sh

--- jobs/generate-jobs/__abci.job.run.sh
+++ jobs/generate-jobs/__abci.job.run.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 module load intel-mpi/2018.2.199
+module load cuda/9.0/9.0.176.2
 
 export LOCAL_HOME=$HOME/local
 export LD_LIBRARY_PATH=$LOCAL_HOME/lib
-export IPP_HOME=$LOCAL_HOME/intel/compilers_and_libraries_2018.1.163/linux/ipp
-export LD_LIBRARY_PATH=$LOCAL_HOME/intel/compilers_and_libraries_2018.1.163/linux/ipp/lib/intel64:$LD_LIBRARY_PATH
-export MPIROOT=/apps/intel/2018.2/compilers_and_libraries_2018.2.199/linux/mpi/intel64
-export PATH=$MPIROOT/bin:$PATH
 export LD_LIBRARY_PATH=$MPIROOT/lib:$LD_LIBRARY_PATH
 export MANPATH=$MANPATH:$MPIROOT/share/man
 
