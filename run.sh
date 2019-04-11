a0966ba58db665a99a72dc66e4220f24c3616f8f
pengdada <chinhou0718@gmail.com>
2019-04-12 00:52:38 +0900

    first

--- run.sh
+++ run.sh
@@ -3,14 +3,24 @@
 # repeate N times
 
 N=1
-echo "each job repeat $N times"
 
-for s in strong2k strong4k strong8k weak2k weak4k weak8k; do
+echo "#########################"
+echo "each job repeat $N times"
+jobs="strong2k strong4k strong8k weak2k weak4k weak8k"
+
+function run_job(){
 	pushd jobs/generate-jobs
 		./clear.sh
-		python gen-jobs.py $s
+		python gen-jobs.py $1
 	popd
+}
+
+
+for s in strong2k strong4k strong8k weak2k weak4k weak8k; do	
+	run_job $s
 	batch-sub-jobs.sh $N
+
+	echo "$s result is in folder test"
 	mkdir -p test
 	mkdir -p test/$s
 	mv *.t1 test/$s
